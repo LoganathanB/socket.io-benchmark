2c1d785796dfe49b6d3f750a245b647821fd8d68
Loganathan <loganathan_balakrishnan@thbs.com>
2015-09-15 17:53:17 +0530

    Namespace support is added in benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,24 +1,24 @@
 var profile = require('v8-profiler');
 var io = require('socket.io-client');
 
-var message = "o bispo de constantinopla nao quer se desconstantinopolizar";
+var message = "{t1p1:{x:152.34, y:243.54, s:3.5}, t2p1:{x:152.34, y:243.54, s:3.5}, t1p2:{x:152.34, y:243.54, s:3.5}, t2p2:{x:152.34, y:243.54, s:3.5}, t1p3:{x:152.34, y:243.54, s:3.5}, t2p3:{x:152.34, y:243.54, s:3.5}, t1g:18, t2g:23}";
 
-function user(shouldBroadcast, host, port) {
-  var socket = io.connect('http://' + host + ':' + port, {'force new connection': true});
+function user(shouldBroadcast, host, port, namespace) {
+  var socket = io.connect('http://' + host + ':' + port + namespace, {'force new connection': true});
 
   socket.on('connect', function() {
-    
+
     // Start messaging loop
     if (shouldBroadcast) {
       // message will be broadcasted by server
-      socket.emit('broadcast', message);
+      socket.emit('message', message);
     } else {
       // message will be echoed by server
       socket.send(message);
     }
 
     socket.on('message', function(message) {
-      socket.send(message);    
+      socket.send(message);
     });
 
     socket.on('broadcastOk', function() {
@@ -26,6 +26,13 @@ function user(shouldBroadcast, host, port) {
     });
   });
 };
+
+var connect = function (ns) {
+  return io.connect(ns, {
+    query: 'ns='+ns,
+    resource: "socket.io"
+  });
+}
 
 var argvIndex = 2;
 
@@ -35,7 +42,8 @@ var newUserTimeout = rampUpTime / users;
 var shouldBroadcast = process.argv[argvIndex++] === 'broadcast' ? true : false;
 var host = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : 'localhost';
 var port = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '3000';
+var namespace = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '';
 
 for(var i=0; i<users; i++) {
-  setTimeout(function() { user(shouldBroadcast, host, port); }, i * newUserTimeout);
+  setTimeout(function() { user(shouldBroadcast, host, port, namespace); }, i * newUserTimeout);
 };
